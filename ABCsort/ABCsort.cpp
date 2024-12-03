a1b52155078f9ea63db5ffd6d210d112021f3ba4
DarkeEnergyy <nightkiller7@mail.ru>
2024-12-03 19:37:29 +0300

    Добавьте файлы проекта.

---
+++ ABCsort/ABCsort.cpp
@@ -0,0 +1,9 @@
+﻿#include <iostream>
+#include "ABCsort_by_Ezhov.h"
+using namespace std;
+
+int main()
+{
+    string str1, str = "Jen Kol Apple Abba Real None Rore";
+    cout << ABCSort(str);
+}
---
+++ ABCsort/ABCsort_by_Ezhov.h
@@ -0,0 +1,13 @@
+#ifndef ABCSORT_H
+#define ABCSORT_H
+
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<string> ABCSort(const vector<string>& collection, int rank);
+
+string ABCSort(const string& input);
+
+#endif // ABCSORT_H
